21aa76d46bcdf70c3a2d2113fa82fe17e7e5a7f2
boniaminbijoy <101921202+boniaminbijoy@users.noreply.github.com>
2026-09-25 18:29:21 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ from tiktok_downloader import (
 # BOT TOKEN
 # ==================================================
 
-BOT_TOKEN = os.environ.get("8645536260:AAH1p9zV8AHfOtZ26l8_oUYjuVwCtIOayH8")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN is missing. Add BOT_TOKEN in Render Environment Variables.")
